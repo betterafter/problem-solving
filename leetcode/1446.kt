178eeecce3c2135e01aa11151b6dc55c820c8823
keykat <koin12000@naver.com>
2021-12-14 02:58:24 +0900

    2021.12.14 프로그래머스, leetcode

---
+++ leetcode/1446.kt
@@ -0,0 +1,20 @@
+class Solution_1 {
+
+    fun maxPower(s: String): Int {
+
+        var max = 1
+        var currMax = 1
+        var curr = s[0]
+        
+        for (i in 1..s.length - 1) {
+            if (curr == s[i]) {
+                currMax += 1
+            } else {
+                curr = s[i]
+                max = Math.max(currMax, max)
+                currMax = 1
+            }
+        }
+        return Math.max(currMax, max)
+    }
+}
