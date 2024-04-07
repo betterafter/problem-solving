add80cea355b8c7241dc612a1b853eac3fcdc6c7
keykat <koin12000@naver.com>
2024-04-08 01:13:57 +0900

    Update 2512.py

--- baekjoon/2512.py
+++ baekjoon/2512.py
@@ -2,17 +2,41 @@
 # 120 110 140 150
 # 485
 
+def calc(li, mid):
+    result = 0
+    for i in li:
+        if (i < mid):
+            result += i
+        else:
+            result += mid
+
+    return result
+
 N = int(input())
 li = list(map(int, input().split()))
 total = int(input())
-target = total // N
 
-cnt = 0
-curr = target * 4
-for i in li: 
-    if (i > target):
-        cnt += 1
-    else:
-        curr -= i
+if (sum(li) < total):
+    print(max(li))
+else:
+    start = 0
+    end = max(li)
+    mid = 0
+    mx = 0
+    while (True):
+        if (start + end) // 2 == mid:
+            break
 
-print(curr / cnt)
+        mid = (start + end) // 2
+
+        if (calc(li, mid) < total):
+            mx = mid
+            start = mid + 1
+
+        elif (calc(li, mid) > total):
+            end = mid - 1
+
+        else:
+            break
+
+    print(mid)
