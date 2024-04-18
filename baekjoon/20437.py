05fde20e2c1c4bf236e72cd6e2e89dff5c3ec925
keykat <koin12000@naver.com>
2024-04-19 01:50:53 +0900

    Update 20437.py

--- baekjoon/20437.py
+++ baekjoon/20437.py
@@ -1,70 +1,34 @@
-import math
+import sys
+input = sys.stdin.readline
 
 T = int(input())
 
 for t in range(T):
-    W = input()
+    W = input().rstrip()
     K = int(input())
 
-    m = {
-        'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0,
-        'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0,
-        'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0,
-        'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
-    }
+    cntDict = {}
+    idxDict = {}
+    
+    answer3 = len(W)
+    answer4 = -1
 
+    check = False
 
-    answer3 = 10000000
-    answer4 = 0
+    for ch in W:
+        cntDict[ch] = cntDict.get(ch, 0) + 1
 
-    left = 0
-    right = 1
-    currW = W[left:right]
-    m[currW[left]] = 1
-    while left < len(W) and right < len(W):
-        # 3번 구하기
-        if K in m.values():
-            answer3 = min(answer3, right - left)
-            if currW[0] == currW[-1]:
-                print(currW)
-                answer4 = max(answer4, len(currW))
-            if left < len(W) - 1:
-                m[W[left]] -= 1
-                left += 1
-        else:
-            if right < len(W) - 1:
-                right += 1
-                m[W[right - 1]] += 1
-            else:
-                m[W[left]] -= 1
-                left += 1
-            
-        currW = W[left:right]
+    for i in range(len(W)):
+        if cntDict[W[i]] >= K:
+            idxDict[W[i]] = idxDict.get(W[i], []) + [i]
+            check = True
+        
+    for keys, values in idxDict.items():
+        for idx in range(len(values) - K + 1):
+            answer3 = min(answer3, values[idx + K - 1] - values[idx] + 1)
+            answer4 = max(answer4, values[idx + K - 1] - values[idx] + 1)
 
-    left = 0
-    right = 1
-    while left < len(W) and right < len(W):
-        # 3번 구하기
-        print(left, right - 1, W[left], W[right - 1])
-        if K in m.values():
-            if currW[0] == currW[-1]:
-                print(currW)
-                answer4 = max(answer4, len(currW))
-                if right < len(W) - 1:
-                    right += 1
-                    m[W[right - 1]] += 1
-            else:
-                if left < len(W) - 1:
-                    m[W[left]] -= 1 
-                    left += 1
-        else:
-            if right < len(W) - 1:
-                right += 1
-                m[W[right - 1]] += 1
-            else:
-                m[W[left]] -= 1
-                left += 1
-            
-        currW = W[left:right]
-
-    print(answer3, answer4)
+    if check:
+        print(answer3, answer4)
+    else:
+        print(-1)
