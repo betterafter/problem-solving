4692f136cb7ecbb10eead4fda700284d6bda6584
keykat <koin12000@naver.com>
2024-04-19 01:55:01 +0900

    Update 20437.py

--- baekjoon/20437.py
+++ baekjoon/20437.py
@@ -7,6 +7,10 @@ for t in range(T):
     W = input().rstrip()
     K = int(input())
 
+    # superaquatornado
+    # idxDict = {
+    # s: [0], u: [1, 7], r: [4, 11], a: [5, 8, 13] 이런식으로 저장됩니다
+    #}
     cntDict = {}
     idxDict = {}
     
@@ -22,9 +26,10 @@ for t in range(T):
         if cntDict[W[i]] >= K:
             idxDict[W[i]] = idxDict.get(W[i], []) + [i]
             check = True
-        
+    
     for keys, values in idxDict.items():
         for idx in range(len(values) - K + 1):
+            # a: [5, 8, 13]이라고 한다면 [5, 8] / [8, 13]을 꺼내면 됩니다.
             answer3 = min(answer3, values[idx + K - 1] - values[idx] + 1)
             answer4 = max(answer4, values[idx + K - 1] - values[idx] + 1)
 
